aa3820a6f95974d11fb149a513f34b6d684eccdf
rgulati31 <148580883+rgulati31@users.noreply.github.com>
2025-08-25 17:45:26 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'   // 👈 this is where your big App lives
-import './index.css'          // 👈 Tailwind styles
+import App from './App.tsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
